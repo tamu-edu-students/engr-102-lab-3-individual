16cae544fdced9e3a2d44ac65545605e3e588b85
Niki Ritchey <65146076+snritchey@users.noreply.github.com>
2025-06-25 10:19:19 -0500

    template code for calling functions

---
+++ calling_functions.py
@@ -0,0 +1,12 @@
+# header goes here
+
+from math import *
+
+def printresult(shape, side, area):
+    '''Print the result of the calculation'''
+    print(f'A {shape} with side {side:.2f} has area {area:.3f}')
+
+# example function call:
+# printresult(<string of shape name>, <float of side>, <float of area>)
+# printresult('square', 2.236, 5)
+# Your code goes here
